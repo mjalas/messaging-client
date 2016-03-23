182564cda2e45f06a91821f160637dfb368f4038
Mats Jalas <mats.jalas@gmail.com>
2016-03-23 15:59:14 +0200

    Added debug mode and direct messaging mode.

--- messaging_client/app.py
+++ messaging_client/app.py
@@ -1,39 +1,29 @@
 from messaging_client.messaging_client import MessagingClient
 
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = 8700
-
 
 class App():
 
     def __init__(self):
         self.client = MessagingClient()
         self.values = self.client.parse_command_line()
+        self.client.set_debug_mode(self.values['debug'])
 
-    def _get_host(self):
-        try:
-            host = self.values['host']
-        except KeyError:
-            host = DEFAULT_HOST
-        return host
-
-    def _get_port(self):
-        try:
-            port = self.values['port']
-        except KeyError:
-            port = DEFAULT_PORT
-        return port
-
-    def run(self, listen_first=False):
-        host = self._get_host()
-        port = self._get_port()
+    def run(self):
+        listen_first = False  # TODO: move this to command line parameter
+        host = self.values['host']
+        port = self.values['port']
         print("Connecting to '" + host + ":" + str(port) + "'...\n")
         self.client.connect(host, port)
         if listen_first:
             response = self.client.receive_response()
             print(response)
+
         print("Sending message...")
-        self.client.send_file_message(self.values['file'])
+        if self.values['file']:
+            self.client.send_file_message(self.values['file'])
+        else:
+            self.client.send_message(self.values['message'])
+
         response = self.client.receive_response()
         print("Received response:")
         print(response)
