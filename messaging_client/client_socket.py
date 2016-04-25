76155c14a4cef4ed7bd7237748ca31e440d0f0ec
Mats Jalas <mats.jalas@gmail.com>
2016-04-25 21:45:50 +0300

    Added control for not being able to send message if not connected.

--- messaging_client/client_socket.py
+++ messaging_client/client_socket.py
@@ -6,11 +6,15 @@ class ClientSocket(object):
     def __init__(self, message_length=1024):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.message_length = message_length
+        self.connected = False
 
     def connect(self, host, port):
         self.socket.connect((host, port))
+        self.connected = True
 
     def send(self, message):
+        if not self.connected:
+            raise UserWarning("Not connected to any remote host.")
         byte_message = str.encode(message)
         self.socket.send(byte_message)
 
@@ -20,3 +24,4 @@ class ClientSocket(object):
 
     def close(self):
         self.socket.close()
+        self.connected = False
